4d040985c8859c6a687bc9d96d3e6464601ef91b
Aswinkumar <aswinkumar.cm@entri.me>
2024-08-05 18:56:18 +0530

    file rename

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -1,12 +1,10 @@
-import express from 'express';
-import userRouter from './userRouter.js'
-import courseRouter from './courseRouter.js'
+import express from "express";
+import userRouter from "./userRoute.js";
+import courseRouter from "./courseRoute.js";
 
 const v1Router = express.Router();
 
-
-
-v1Router.use('/user', userRouter )
-v1Router.use('/course', courseRouter  )
+v1Router.use("/user", userRouter);
+v1Router.use("/course", courseRouter);
 
 export default v1Router;
