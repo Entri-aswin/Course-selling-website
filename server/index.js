a981cb1b642e5169f56ace99314aa994f7543774
Aswinkumar <aswinkumar.cm@entri.me>
2024-08-09 14:59:16 +0530

    prep for deployment

--- server/index.js
+++ server/index.js
@@ -1,30 +1,26 @@
-import express from 'express'
-import { connectDB } from './config/db.js';
-import apiRouter from './routes/index.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import { connectDB } from "./config/db.js";
+import apiRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
 const port = process.env.PORT;
 
-connectDB()
+connectDB();
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
-app.get('/',  (req, res) => {
-  res.send('Hello World!')
+app.use("/api", apiRouter);
 
-})
-
-
-app.use('/api', apiRouter)
-
-
+app.all("*", (req, res, next) => {
+    res.status(404).json({ message: "end point does not exist" });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-
+    console.log(`Example app listening on port ${port}`);
+});
