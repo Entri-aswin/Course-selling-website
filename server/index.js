251cdda459ccbb7889395a47b2537c1424af2d65
Aswinkumar <aswinkumar.cm@entri.me>
2024-08-19 20:51:34 +0530

    cors error update

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://elearning-pjvtuibps-entri-aswins-projects.vercel.app",
         credentials: true,
     })
 );
